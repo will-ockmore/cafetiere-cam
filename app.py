f80889a937b8501413a33a863a4cce7456ba5a8a
Will Ockmore <will.ockmore@gmail.com>
2017-01-26 14:00:26 +0000

    loop recording

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+from datetime import datetime
 from picamera import PiCamera
 
 camera = PiCamera()
 camera.resolution = (640, 480)
 
-camera.start_recording('my_video.h264')
-camera.wait_recording(60)
-camera.stop_recording()
+# camera.start_recording('my_video.h264')
+# camera.wait_recording(60)
+# camera.stop_recording()
 
+while True:
+    timestamp = datetime.now().strftime('%m-%d_%H:%M')
+
+    camera.start_recording('/var/www/html/{}.h264'.format(timestamp))
+    camera.wait_recording(10)
+    camera.stop_recording()
